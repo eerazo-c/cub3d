7d0da5aea4978b7efdc80110261dd7d2dbb23634
Elizabeth Nayeli Erazo Chacon <elerazo-@cbr13s6.42barcelona.com>
2026-01-09 18:55:31 +0100

    atoi add

---
+++ src/b.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int	ft_atoi_base(const char *str, int str_base)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+	int c;
+
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			c = str[i] - '0';
+		else if (str[i] >= 'a' && str[i] <= 'f')
+			c = str[i] - 'a' + 10;
+		else if (str[i] >= 'A' && str[i] <= 'F')
+			c = str[i] - 'A' + 10;
+		else 
+			break;
+		result = result * str_base + c;
+		i++;
+	}
+	return (result * sign);
+}
+
+int main(int ac, char **av)
+{
+	if (ac == 2)
+		printf("result: %d\n", ft_atoi_base(av[1], 16));
+	return (0);
+}
