106df7efa8c5e6a4c049297f172e7cd47250e0fd
Elizabeth Nayeli Erazo Chacon <elerazo-@cbr13s6.42barcelona.com>
2026-01-09 17:08:04 +0100

    test all

--- src/parse_cub.c
+++ src/parse_cub.c
@@ -77,7 +77,7 @@ void	parse_cub(char *file, t_map *game)
 	//	printf("LINEA LEIDA -> [%s]\n", line);
 		parse_line(line, game);
 		free(line);
-		line = get_next_line(fd);
+		line = get_next_lin e(fd);
 	}
 	close(fd);
 }
