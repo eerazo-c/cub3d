4bf5b06dbd75acc5881b5fd063bdbadf95f07918
Elizabeth Nayeli Erazo Chacon <elerazo-@car8s2.42barcelona.com>
2026-01-22 18:28:25 +0100

    funcion aparte de readline

---
+++ src/cubparse.c
@@ -0,0 +1,46 @@
+#include "cub3d.h"
+
+char *read_line(int fd)
+{
+	char	*buffer;
+	char	xchar;
+	int		byte;
+	int		index;
+
+	index = 0;
+	byte = read(fd, &xchar, 1);
+	buffer = malloc(sizeof(char) * 2);
+	while (byte > 0)
+	{
+		if (xchar == '\n')
+			break ;
+		buffer[index++] = xchar;
+		buffer = ft_realloc(buffer, ft_strlen(buffer) + 1, ft_strlen(buffer) + 2);
+		byte = read(fd, &xchar, 1);
+	}
+	if ((byte <= 0) && (index == 0))
+		return (free(buffer), NULL);
+	buffer[index] = '\0';
+	return (buffer);
+}
+
+void	parse_cub(char *file, t_map game)
+{
+	(void)game;
+	int		fd;
+	char	*line;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		ft_error_fd("ERROR: cannot open file", 1);
+
+	line = read_line(fd);
+	while (line)
+	{
+		printf("LINEA LEIDA -> [%s]\n", line);
+		//parse_line(line, game);
+		free(line);
+		line = read_line(fd);
+	}
+	close(fd);
+}
